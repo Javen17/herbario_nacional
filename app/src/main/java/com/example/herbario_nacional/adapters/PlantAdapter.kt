5c05cc44a2491dfe40c71b96520fbd1b7e0ac371
Marcos Sevilla <marcossevilla14@gmail.com>
2020-03-20 07:30:06 -0600

    Moved FAB and added search button

--- app/src/main/java/com/example/herbario_nacional/adapters/PlantAdapter.kt
+++ app/src/main/java/com/example/herbario_nacional/adapters/PlantAdapter.kt
@@ -1,16 +1,9 @@
 package com.example.herbario_nacional.adapters
 
-import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
 import com.example.herbario_nacional.models.Plant
-import com.example.herbario_nacional.R
 import com.example.herbario_nacional.imageloader.ImageLoader
 import ni.abril.azb.megaboicotapp.ui.view_holders.PlantViewHolder
 
