e6724da5d30a5cbf86693e5ca9bd994cf12d3224
Bryan Barrios <bryan.perez@est.uca.edu.ni>
2020-04-12 15:33:19 -0600

    Login establecido como activity inicial

--- app/src/main/java/com/example/herbario_nacional/ui/Activities/NoLoginActivity.kt
+++ app/src/main/java/com/example/herbario_nacional/ui/Activities/NoLoginActivity.kt
@@ -11,14 +11,15 @@ class NoLoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_no_login)
-        close_btn.setOnClickListener { showActivity(MainActivity::class.java) }
-        register_btn.setOnClickListener { showActivity(RegisterActivity::class.java) }
-        login_btn.setOnClickListener { showActivity(LoginActivity::class.java) }
+        register_btn.setOnClickListener { showActivity(RegisterActivity::class.java, false) }
+        login_btn.setOnClickListener { showActivity(LoginActivity::class.java, false) }
+        guess_btn.setOnClickListener { showActivity(MainActivity::class.java, true) }
     }
 
-    private fun showActivity(activityClass: Class<*>) {
+    private fun showActivity(activityClass: Class<*>, isFinish: Boolean) {
         val intent = Intent(this, activityClass)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
         startActivity(intent)
+        if (isFinish) finish()
     }
 }
