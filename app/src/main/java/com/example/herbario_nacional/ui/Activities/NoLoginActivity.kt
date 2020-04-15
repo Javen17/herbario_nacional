3e0fdbb02646dde3dbf36da7b0ae38ebd1c9fb05
Bryan Barrios <bryan.perez@est.uca.edu.ni>
2020-04-15 13:49:24 -0600

    Cambios mínimos de apariencia

--- app/src/main/java/com/example/herbario_nacional/ui/Activities/NoLoginActivity.kt
+++ app/src/main/java/com/example/herbario_nacional/ui/Activities/NoLoginActivity.kt
@@ -2,15 +2,20 @@ package com.example.herbario_nacional.ui.Activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.example.herbario_nacional.R
+import com.example.herbario_nacional.ui.viewModels.MeViewModel
 import kotlinx.android.synthetic.main.activity_no_login.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class NoLoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_no_login)
+
         register_btn.setOnClickListener { showActivity(RegisterActivity::class.java, false) }
         login_btn.setOnClickListener { showActivity(LoginActivity::class.java, false) }
         guess_btn.setOnClickListener { showActivity(MainActivity::class.java, true) }
